920416f0a71d4878de24a6e25271e227398c1ac7
David Isebarn <isebarn182@gmail.com>
2021-08-20 09:05:03 +0000

    test

--- api/index.js
+++ api/index.js
@@ -39,7 +39,7 @@ app.get('/spending', async (req, res) => {
     let summarySheet = doc.sheetsByTitle['Summary']
     const rows = await summarySheet.getRows();*/
     res.json({
-      projection: "asd",
+      projection: process.env.SERVICE_LOCK,
     })
 
   } catch (err) {
