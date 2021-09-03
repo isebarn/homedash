7a72aa710248b95c81894e4afa5e814b67390d01
David Isebarn <isebarn182@gmail.com>
2021-09-03 10:37:58 +0000

    test endpoint

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 const fetch = require('node-fetch')
 const cheerio = require('cheerio')
+const { Curl } = require("node-libcurl");
+const querystring = require("querystring");
 const cron = require('node-cron');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 var MongoClient = require('mongodb').MongoClient
@@ -14,11 +16,33 @@ app.use(express.json());
 
 // cron.schedule('* * * * *', () => {console.log("Task is running every minute " + new Date())});
 
+app.get('/test', async (req, res) => {
+  const curlTest = new Curl();
+  const terminate = curlTest.close.bind(curlTest);
+  curlTest.setOpt(Curl.option.URL, "https://www.oddur.is");
+
+  curlTest.on("end", function (statusCode, data, headers) {
+    const parsed = cheerio.load(data)
+    const text = parsed('.topnav').children('h2:first').text().trim()
+    const values = text.split(' - ')
+    const temperature = values[0]
+    const wind = values[1].split(' ')[1]
+    res.json({
+      temperature,
+      wind
+  })
+  this.close();
+  });
+  curlTest.on("error", terminate);
+
+  curlTest.perform();
+})
+
 
 app.get('/data', async (req, res) => {
   return fetch('https://www.oddur.is')
-    .then(response => { res.json({temperature: 1, wind: 1})})
-/*    .then((data) => {
+    .then(response => response.text())
+    .then((data) => {
       const parsed = cheerio.load(data)
       const text = parsed('.topnav').children('h2:first').text().trim()
       const values = text.split(' - ')
@@ -28,7 +52,7 @@ app.get('/data', async (req, res) => {
         temperature,
         wind
       })
-    })*/
+    })
 })
 
 app.get('/spending', async (req, res) => {
